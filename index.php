d07d098e172513edaaac5e392014d827b2312a1e
YoujoSenku <scl491455@gmail.com>
2026-04-14 15:47:33 +0700

    Update

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Kisi - Kisi</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Raharja Sawit</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav nav-underline justify-content-end ms-auto gap-2">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hasil Kebun</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hubungi Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="hero-section text-center">
+        <div class="container d-block mt-5 pb-5 ">
+            <h1 class="display-3 fw-bold">Nyawit Raharja</h1>
+            <p class="lead hero-subtitle">
+                Mendedikasikan Hidup untuk Kelapa Sawit yang Berkelanjutan dan Berkualitas
+            </p>
+            <button type="button" class="btn btn-primary rounded-5 px-4 py-2 mt-3">
+                Kenali Saya Lebih Dekat
+            </button>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <main>
+        <!-- About Section -->
+        <div class="container text-center py-5">
+            <div class="row gx-5 align-items-center">
+                <!-- Gambar Profil : Kiri -->
+                <div class="col-md-6 col-lg-5">
+                    <div class="col-md-5 text-center">
+                        <div class="about-img-wrapper">
+                            <!-- Ganti src dengan foto Anda sendiri -->
+                            <img src="https://images.unsplash.com/photo-1625246333195-78d9c38ad449?w=500&q=80"
+                                alt="Foto Kebun Sawit"
+                                class="about-img"/>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Gambar Profil : Kanan -->
+                <div class="col-md-6 col-lg-7 text-center">
+                    <div class="fw-bold mt-4">Siapa Nyawit Raharja</div>
+                    <p class="text-paragraph text-muted">
+                        Saya adalah Seorang petani kelapa sawit yang telah bergelut di bidang perkebunan selama lebih dari 20 tahun. Berfokus di wilayah Sumatera, saya menerapakan teknik pertanian modern yang ramah lingkungan.
+                    </p>
+                    <p class="text-paragraph text-muted">
+                        Misi saya adalah membuktikan bahwa perkebunan sawit rakyat dapat dikelola secara profesional dengan hasil panen (TBS) kualitas premium yang memenuhi standar industri global.
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        
+    </footer>
+
+    <!-- Script Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
